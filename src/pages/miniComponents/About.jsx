dcfc79bbac1e85261b98a051c7e5afb9dc283cb0
nabihakhan6t4 <khannabiha923@gmail.com>
2025-05-08 15:16:59 +0300

    chanegs

--- src/pages/miniComponents/About.jsx
+++ src/pages/miniComponents/About.jsx
@@ -25,22 +25,38 @@ const About = () => {
         <div className="grid md:grid-cols-2 my-8 sm:my-20 gap-14">
           <div className="flex justify-center items-center">
             <img
-              src="src/images/nabiha-black.jpg"
+              src="/nabiha-black.jpg"
               alt="avatar"
               className="bg-white p-2 sm:p-4 rotate-[25deg] h-[240px] sm:h-[340px] md:h-[350px] lg:h-[450px]"
             />
           </div>
           <div className="flex justify-center flex-col tracking-[1px] text-xl gap-5">
             <p>
-            Hey! I’m Nabiha Khan, a passionate and detail-oriented Frontend Developer from Karachi, Pakistan 🌍. I specialize in building modern, responsive, and visually stunning websites using HTML, CSS, JavaScript, React, Bootstrap, and Tailwind CSS 💻. Every project is a journey for me to solve complex problems and transform ideas into beautiful digital solutions ✨. Whether it’s a simple webpage or an intricate design, I strive to make every line of code count 💡.
+              Hey! I’m Nabiha Khan, a passionate and detail-oriented Frontend
+              Developer from Karachi, Pakistan 🌍. I specialize in building
+              modern, responsive, and visually stunning websites using HTML,
+              CSS, JavaScript, React, Bootstrap, and Tailwind CSS 💻. Every
+              project is a journey for me to solve complex problems and
+              transform ideas into beautiful digital solutions ✨. Whether it’s
+              a simple webpage or an intricate design, I strive to make every
+              line of code count 💡.
             </p>
             <p>
-            Right now, I’m diving deep into the MERN stack 🔍, sharpening my skills to work on full-stack projects that enhance both user experience and backend functionality 🚀. My focus is on creating robust applications that offer dynamic and responsive web experiences. I’m exploring new technologies and working on real-world projects to expand my toolset ⚙️.
+              Right now, I’m diving deep into the MERN stack 🔍, sharpening my
+              skills to work on full-stack projects that enhance both user
+              experience and backend functionality 🚀. My focus is on creating
+              robust applications that offer dynamic and responsive web
+              experiences. I’m exploring new technologies and working on
+              real-world projects to expand my toolset ⚙️.
             </p>
           </div>
         </div>
         <p className="tracking-[1px] text-xl">
-        Looking ahead, my goal is to evolve into a professional full-stack developer 👩‍💻, creating impactful web applications that solve real problems 🌱. I believe in continuous learning 📚 and love collaborating on projects that push me to grow 💪. Every challenge is an opportunity to learn and improve, and I’m here for it 💫.
+          Looking ahead, my goal is to evolve into a professional full-stack
+          developer 👩‍💻, creating impactful web applications that solve real
+          problems 🌱. I believe in continuous learning 📚 and love
+          collaborating on projects that push me to grow 💪. Every challenge is
+          an opportunity to learn and improve, and I’m here for it 💫.
         </p>
       </div>
     </div>
